5ee8b1091ecc137c74c031a7385af4d4df5a5c2c
Damoon <damoon.shah3@gmail.com>
2024-11-29 09:23:07 -0500

    minor

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/civil-asin/', // Replace with your repository name
+  base: '/civil-asin', // Replace with your repository name
   plugins: [react()],
 });
