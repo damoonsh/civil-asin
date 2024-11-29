d18befb37a2d2af94a1c4a232e164725dcb8c62e
Damoon <damoon.shah3@gmail.com>
2024-11-29 09:04:01 -0500

    init

---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+// src/Header.jsx
+import './Header.css'; // Optional: for styling
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="../public/civil_asin_logo.png" alt="Company Logo" className="logo" />
+      <h1>Civil Asin</h1>
+    </header>
+  );
+};
+
+export default Header;
